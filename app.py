7ec2a301619346aa0afeffb0df8b99bc159a2724
Very Simbolon <verisimb@gmail.com>
2026-06-22 19:49:00 +0800

    Update model to newly trained RF and K-Means models and update Gradio app

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # Load models and scaler
-model_rf = joblib.load('model_rf.pkl')
-model_kmeans = joblib.load('model_kmeans.pkl')
-scaler = joblib.load('scaler.pkl')
+rf_data = joblib.load('random_forest_crop_recommendation.pkl')
+model_rf = rf_data['model']
+
+kmeans_data = joblib.load('kmeans_land_clustering.pkl')
+model_kmeans = kmeans_data['model']
+scaler = kmeans_data['scaler']
+cluster_label_map = kmeans_data.get('cluster_label_map', {})
 
 def predict(n, p, k, suhu, kelembapan, ph, curah_hujan):
     # Data input mentah
@@ -20,13 +24,8 @@ def predict(n, p, k, suhu, kelembapan, ph, curah_hujan):
     input_scaled = scaler.transform(input_data)
     kmeans_pred = model_kmeans.predict(input_scaled)[0]
     
-    cluster_meaning = {
-        0: "Lahan Lembap/Basah",
-        1: "Lahan Kering/Panas",
-        2: "Lahan Sedang/Stabil"
-    }
-
-    hasil_cluster = f"Cluster {kmeans_pred} ({cluster_meaning.get(kmeans_pred, 'Tidak diketahui')})"
+    label_tipe_lahan = cluster_label_map.get(kmeans_pred, "Tidak diketahui")
+    hasil_cluster = f"Cluster {kmeans_pred} ({label_tipe_lahan})"
     
     return rf_pred, hasil_cluster
 
